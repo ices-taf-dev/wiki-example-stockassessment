188c01906b59cf322553e2c9e6ca2f23f2aa56cf
Colin Millar <colin.millar@ices.dk>
2023-01-22 15:58:05 +0100

    add config, remove fit and switch stock

--- bootstrap/sam_config.R
+++ bootstrap/sam_config.R
@@ -1,20 +1,13 @@
-
-library(icesTAF)
-
 sam_assessment <- "Cod_27_5b1"
 
 sam_dir <-
   paste0(
     "https://stockassessment.org/datadisk/stockassessment/userdirs/user3/",
     sam_assessment,
-    "/data/"
+    "/conf/"
   )
 
-files <-
-  paste0(
-    c("cn", "cw", "dw", "lf", "lw", "mo", "nm", "pf", "pm", "survey", "sw"),
-    ".dat"
-  )
+files <- "model.cfg"
 
 for (file in files) {
   download(paste0(sam_dir, file))
--- bootstrap/sam_fit.R
+++
@@ -1,8 +0,0 @@
-
-library(stockassessment)
-
-# download model from stockassessment.org
-fit <- fitfromweb("WBCod_2021_cand01")
-
-# save to model folder
-save(fit, file = "fit.RData")
